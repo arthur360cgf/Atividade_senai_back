98aac481c67f9a1f48ab4a60159f92430ed9acfa
Arthur Aragão Mattei Moraes <68078154+arthur360cgf@users.noreply.github.com>
2024-09-09 20:58:59 -0300

    Add files via upload

---
+++ Projeto_Web-Lh_Pets_Alunos/Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Projeto Web - LH Pets versão 1");
+
+app.UseStaticFiles();
+
+Banco banco = new Banco();
+
+app.MapGet("/listaClientes", (HttpContext context) => {
+    context.Response.WriteAsync(banco.GetListaString());
+});
+
+app.Run();
